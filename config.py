dc4c0c0b4c57286096d7acc044c8fcbb8a3a36dc
Vedashree <vedashree358@gmail.com>
2026-02-15 17:52:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,12 +5,13 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'smart-stock-system-2025-vedashree')
     
     # Database logic: Check Cloud first, then check Local
-    MYSQL_HOST = os.getenv('MYSQLHOST', 'localhost')
-    MYSQL_USER = os.getenv('MYSQLUSER', 'root')
+    MYSQL_HOST = os.getenv('MYSQLHOST', 'mysql-3af63198-vedashree358-583a.j.aivencloud.com')
+    MYSQL_USER = os.getenv('MYSQLUSER', 'avnadmin')
     # Change 'root' below to your actual local XAMPP password if it's different
-    MYSQL_PASSWORD = os.getenv('MYSQLPASSWORD', 'root') 
+    MYSQL_PASSWORD = os.getenv('MYSQLPASSWORD', 'AVNS_eKVKKpSgRbkDtX9DHpP') 
     MYSQL_DB = os.getenv('MYSQLDATABASE', 'smart_stock')
-    MYSQL_PORT = int(os.getenv('MYSQLPORT') or 3306)
+    MYSQL_PORT = int(os.getenv('MYSQLPORT') or 18369)
+    MYSQL_SSL_CA = os.path.join(os.getcwd(), 'ca.pem') if 'aivencloud.com' in MYSQL_HOST else None
     
     MYSQL_CURSORCLASS = 'DictCursor'
     UPLOAD_FOLDER = 'uploads'
